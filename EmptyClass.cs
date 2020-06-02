97b609ef351f26584a80979c6645de5211073c38
rveera264 <rveeravelli93@gmail.com>
2020-06-02 16:48:37 -0500

    First Commit to new Udemy Git

---
+++ EmptyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Application
+{
+    public class DemoEmptyClass
+    {
+        public EmptyClass(){
+            
+
+            Console.WriteLine("This is an empty class");
+        }
+    }
+}
